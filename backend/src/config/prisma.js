a415292b587783cad69f3db0b7ffb43284b4bc27
Thanawat Kongsisang <thanawatkongsisang@gmail.com>
2026-06-21 01:31:34 +0700

    Generate prisma client on deploy

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,14 +1,7 @@
 require("dotenv").config();
 
-const { PrismaClient } = require("../generated/prisma");
-const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
+const { PrismaClient } = require("@prisma/client");
 
-const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL,
-});
-
-const prisma = new PrismaClient({
-    adapter,
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
